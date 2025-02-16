5fdf27767e561d0ca6867db2c5be60b7424bd6de
Elizaveta <nikolaevaliz00@m1nt.tech>
2025-02-16 23:08:33 +0300

    Changes for 2 if 3 task

--- hw7/hh7.py
+++ hw7/hh7.py
@@ -49,9 +49,6 @@ if minutes_int < 10:
 else:
     minutes_str = str(minutes_int)
 
-if hours_int < 10:
-    time_12 = str(hours_int) + ':' + minutes_str + ' ' + period
-else:
-    time_12 = str(hours_int) + ':' + minutes_str + ' ' + period
+time_12 = str(hours_int) + ':' + minutes_str + ' ' + period
 
 print(time_12)
