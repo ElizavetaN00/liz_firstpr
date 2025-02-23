bb148d9e4d00bfe8c2202486562a165e75e1bf77
Elizaveta <nikolaevaliz00@m1nt.tech>
2025-02-23 22:46:21 +0300

    Homework 9

---
+++ hw9/hw9.py
@@ -0,0 +1,53 @@
+def solution(sequence):
+    count = 0
+    for i in range(1, len(sequence)):
+        if sequence[i] <= sequence[i - 1]:
+            count +=1
+        if count > 1:
+                return False
+        if i >= 2 and sequence[i] <= sequence[i - 2]:
+                sequence[i] = sequence[i - 1]
+        else:
+                sequence[i - 1] = sequence[i]
+    return True
+
+assert solution([1, 2, 3]) == True
+assert solution([1, 2, 1, 2]) == False
+assert solution([1, 3, 2, 1]) == False
+assert solution([1, 2, 3, 4, 5, 3, 5, 6]) == False
+assert solution([40, 50, 60, 10, 20, 30]) == False
+
+print('Successful1')
+
+def solution(n, f_number):
+    opp_number = (f_number + n//2) % n
+    return opp_number
+
+assert solution(10, 6) == 1
+assert solution(10, 2) == 7
+assert solution(10, 4) == 9
+
+print('Successful2')
+
+def solution(number):
+    number_of_card = str(number)
+    if not number_of_card.isdigit() or number_of_card == "":
+        return False
+    number_of_card = number_of_card[::-1]
+    sum = 0
+    for i in range(len(number_of_card)):
+        int_digit = int(number_of_card[i])
+        if i % 2 == 1:
+            int_digit *= 2
+            if int_digit > 9:
+                int_digit -= 9
+        sum += int_digit
+
+    return sum % 10 == 0
+
+assert solution(4561261212345464) == False
+assert solution(4561261212345467) == True
+assert solution("") == False
+assert solution(6011111111111117) == True
+
+print('Successful3')
