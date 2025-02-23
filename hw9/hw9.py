814edc289c16b93102bd2572928d324e070a1d7a
Elizaveta <nikolaevaliz00@m1nt.tech>
2025-02-23 22:59:18 +0300

    Homework 9

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -11,25 +11,26 @@ def solution(sequence):
                 sequence[i - 1] = sequence[i]
     return True
 
-assert solution([1, 2, 3]) == True
-assert solution([1, 2, 1, 2]) == False
-assert solution([1, 3, 2, 1]) == False
-assert solution([1, 2, 3, 4, 5, 3, 5, 6]) == False
-assert solution([40, 50, 60, 10, 20, 30]) == False
+assert solution([1, 2, 3]) # True
+assert not solution([1, 2, 1, 2]) # False
+assert not solution([1, 3, 2, 1]) # False
+assert not solution([1, 2, 3, 4, 5, 3, 5, 6]) # False
+assert not solution([40, 50, 60, 10, 20, 30]) # False
 
 print('Successful1')
 
-def solution(n, f_number):
+def solution2(n, f_number):
+
     opp_number = (f_number + n//2) % n
     return opp_number
 
-assert solution(10, 6) == 1
-assert solution(10, 2) == 7
-assert solution(10, 4) == 9
+assert solution2(10, 6) == 1
+assert solution2(10, 2) == 7
+assert solution2(10, 4) == 9
 
 print('Successful2')
 
-def solution(number):
+def solution3(number):
     number_of_card = str(number)
     if not number_of_card.isdigit() or number_of_card == "":
         return False
@@ -45,9 +46,9 @@ def solution(number):
 
     return sum % 10 == 0
 
-assert solution(4561261212345464) == False
-assert solution(4561261212345467) == True
-assert solution("") == False
-assert solution(6011111111111117) == True
+assert not solution3(4561261212345464) # False
+assert solution3(4561261212345467) # True
+assert not solution3("") # False
+assert solution3(6011111111111117) # True
 
 print('Successful3')
