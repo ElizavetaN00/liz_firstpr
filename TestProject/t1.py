0395e80261bee2fe9e8cee5f6ca9e839897a5699
Elizaveta <nikolaevaliz00@m1nt.tech>
2025-03-18 19:32:11 +0300

    Task1

---
+++ TestProject/t1.py
@@ -0,0 +1,18 @@
+# 1
+str1 = "Never regret anything that made you smile"
+first_symbol = str1[0]
+last_symbol = str1[-1]
+third_from_beginning_symbol = str1[2]
+third_from_end_symbol = str1[-3]
+str1_length = len(str1)
+reversed_str1 = str1[::-1]
+first_eight_symbols = str1[:8]
+
+print(f"My string is: {str1}")
+print(f"First symbol is: {first_symbol}")
+print(f"Last symbol is: {last_symbol}")
+print(f"Third from beginning symbol is: {third_from_beginning_symbol}")
+print(f"Third from end symbol is: {third_from_end_symbol}")
+print(f"The length of '{str1}' is: {str1_length}")
+print(f"Reversed string is: {reversed_str1}")
+print(f"First eight symbols are: {first_eight_symbols}")
